edddddbe46d8404446bc0ff9abce3399bed5c97d
Joshua Oyewole <joshpp2013@gmail.com>
2021-12-30 02:14:36 +0100

    Navbar and slideshow section created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import moreThanVisual from './images/more-than-visual.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <section className="section-2 grid-2 container">
+        <div className="img__section">
+          <img src={moreThanVisual} alt="More than visual design, more than interaction" className='morethanvisualImg' />
+        </div>
+          <div className="text">
+            <h2 className="primary__header">
+              More than visual design, more than interactions.
+            </h2>
+            <p className="details">
+              We take pride in our craft. Drawing on our deep expertise in design, our Product Design team cares for your user's experience over the entire customer journey, at every touchpaint with your company.
+            </p>
+            <div className="details">
+              We shepherd your product through the entire design process. We aim to achieve your goals.
+            </div>
+            <p className="details">
+              We also collaborate with our software Engineering team to ensure our designs are technically feasible within the constraints of the project. At the same time, we're watching for innovative new technologies that can maek the experience more seamless.
+            </p>
+          </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul className="nav__list">
+                <li className="nav__item uppercase"><a href="/">Home</a></li>
+                <li className="nav__item uppercase"><a href="/page">Page 2</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
